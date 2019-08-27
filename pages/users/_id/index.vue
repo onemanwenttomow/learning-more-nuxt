daaaff99a80f87452b50948678560bf6aee1854a
Peter Anderson <pete.anderson@gmail.com>
2019-08-27 20:59:42 +0200

    First commit

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>A singer user, with ID: {{ $route.params.id }}</h1>
+</template
---
+++ pages/users/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="">
+        <h1>My users component</h1>
+        <input type="text" v-model="userId">
+        <button @click="loadUser">Load User</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userId: ''
+        }
+    },
+    methods: {
+        loadUser() {
+            this.$router.push(`/users/${this.userId}`)
+        }
+    }
+}
+
+</script>
